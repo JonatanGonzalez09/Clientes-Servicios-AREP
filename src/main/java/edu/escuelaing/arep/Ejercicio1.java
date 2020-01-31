fa1df235da0702ba2714c936ee1a120355cbf3ea
JonatanGonzalez09 <jonatan0924@gmail.com>
2020-01-31 12:39:22 -0500

    avance

---
+++ src/main/java/edu/escuelaing/arep/Ejercicio1.java
@@ -0,0 +1,52 @@
+package edu.escuelaing.arep;
+
+import java.net.*;
+
+class GFG { 
+    public static void main(String args[]) 
+    { 
+        // url object 
+        URL url = null; 
+  
+        try { 
+            // create a URL 
+            url = new URL("https://pegaso.escuelaing.edu.co/index.html?0=10&a=15#AREP"); 
+            //url = new URL("http://campusvirtual.escuelaing.edu.co/moodle/"); 
+  
+            // get the Protocol 
+            String Protocol = url.getProtocol(); 
+            System.out.println("Protocol = " + Protocol); 
+
+            // get the Autority 
+            String Autority = url.getAuthority(); 
+            System.out.println("Autority = " + Autority); 
+
+            //getHost
+            String  Host = url.getHost();
+            System.out.println("Host = " + Host);
+
+            //getPort
+            int  Port = url.getPort();
+            System.out.println("Port = " + Port);
+
+            //getPath
+            String  Path = url.getPath();
+            System.out.println("Path = " + Path);
+
+            //getQuery
+            String Query = url.getQuery();
+            System.out.println("Query = " + Query);
+
+            //getFile
+            String File = url.getFile();
+            System.out.println("File = " + File);
+
+            //getRef
+            String Ref = url.getRef();
+            System.out.println("Ref = " + Ref);
+
+        } catch (Exception e){ 
+            System.out.println(e); 
+        } 
+    } 
+} 
---
+++ src/main/java/edu/escuelaing/arep/Ejercicio2.java
@@ -0,0 +1,29 @@
+package edu.escuelaing.arep;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+//Browser
+public class Ejercicio2{
+
+    public static void main(String[] args) throws MalformedURLException {
+        Scanner entradaEscaner = new Scanner (System.in); //Creación de un objeto Scanner
+        String entradaTeclado = entradaEscaner.nextLine(); // Invocamos un método sobre un objeto Scanner
+        System.out.println ("Entrada recibida por teclado es: \"" + entradaTeclado +"\"");
+        URL url = new URL(entradaTeclado);
+        try (BufferedReader reader= new BufferedReader(new InputStreamReader(url.openStream()))) {
+            File res = new File("resultado.html");
+            FileWriter f = new FileWriter(res, true);
+            String inputLine = null;
+            while ((inputLine = reader.readLine()) != null){
+                //System.out.println(inputLine);
+                f.write(inputLine);
+            }
+            f.close();
+        }catch (IOException x){ 
+            System.err.println(x);
+        }
+        entradaEscaner.close();
+    }
+}
