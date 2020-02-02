f74cab1934ec4eb0d448aaa6fd30427451d7fd3e
JonatanGonzalez09 <jonatan0924@gmail.com>
2020-02-02 00:16:34 -0500

    Reto 1

--- src/main/java/edu/escuelaing/arep/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/HttpServer.java
@@ -1,37 +1,50 @@
 package edu.escuelaing.arep;
 
 import java.net.*;
+
 import java.io.*;
+
+
 public class HttpServer {
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(35000);
-        } catch (IOException e) {
-            System.err.println("Could not listen on port: 35000.");
-            System.exit(1);
-        }
-        Socket clientSocket = null;
-        try {
-            System.out.println("Listo para recibir ...");
-            clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-            System.err.println("Accept failed.");
-            System.exit(1);
-        }
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String inputLine, outputLine;
-        while ((inputLine = in.readLine()) != null) {
-            System.out.println("Received: " + inputLine);
-            if (!in.ready()) {
-                break;
+
+    /**
+         * Metodo main, metodo principal de la clase HttpServer.
+         * Crea un servidor y un cliente Socket, y asigna un puerto
+         * para el servidor web.
+         * 
+         * @param args
+         * @throws IOException
+         */
+        public static void main(String[] args) throws IOException {
+            ServerSocket serverSocket = null;
+            Integer port;
+            try { 
+                // Puerto desde una variable de entorno
+                port = new Integer(System.getenv("PORT"));
+            } catch (Exception e) {
+                port = 35000;
             }
+            
+            try {
+                serverSocket = new ServerSocket(port);
+            } catch (IOException e) {
+                System.err.println("Could not listen on port: 35000.");
+                System.exit(1);
+            }
+            
+    
+            Socket clientSocket = null;
+            while (true) {
+                try {
+                    System.out.println("Listo para recibir ...");
+                    clientSocket = serverSocket.accept();
+                } catch (IOException e) {
+                    System.err.println("Accept failed.");
+                    System.exit(1);
+                }
+                Multisolicitud multi = new Multisolicitud(clientSocket);
+                multi.start();
+            }
+    
         }
-        outputLine = "<!DOCTYPE html>"+ "<html>"+ "<head>"+ "<meta charset=\"UTF-8\">"+ "<title>Title of the document</title>\n"+ "</head>"+ "<body>"+ "My Web Site"+ "</body>"+ "</html>" + inputLine;out.println(outputLine);
-        out.close();
-        in.close();
-        clientSocket.close();
-        serverSocket.close();
     }
-}
