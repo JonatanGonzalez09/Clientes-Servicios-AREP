c7213a3df284c4cf0ffc539f8b4124ebc6e05429
JonatanGonzalez09 <jonatan0924@gmail.com>
2020-02-06 14:32:37 -0500

    Reto 1 Finalizado

--- src/main/java/edu/escuelaing/arep/reto1/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/reto1/HttpServer.java
@@ -5,159 +5,143 @@ import java.io.*;
 
 public class HttpServer {
 
-    private int puerto;
-    private Socket clientSocket;
-    private ServerSocket servSocket;
-    private String serverMessage;
+	private int puerto;
+	private Socket clientSocket;
+	private static ServerSocket serverSocket;
+	private String mensaje;
 
-    /**
-     * Class constructor
-     * 
-     * @throws IOException
-     */
-    public HttpServer() {
-        this.puerto = getPort();
-        try {
-            this.servSocket = new ServerSocket(this.puerto);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-	}
-	
 	/**
-	 * This method starts the server
+	 * Clase constructor
+	 * 
+	 * @throws IOException
 	 */
-	public void startServer() {
-		while(true) {
+	public HttpServer() {
+		this.puerto = getPort();
+		try {
+			HttpServer.serverSocket = new ServerSocket(this.puerto);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Metodo para empezar a correr el servidor
+	 */
+	public void run() {
+		while (true) {
 			try {
-                this.clientSocket=this.servSocket.accept();
-				
-				BufferedReader entrada= new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
-				
-				String path=this.getRequest(entrada);
-				
-                OutputStream socketClient=clientSocket.getOutputStream();
-                
-				if("/webPage".equals(path)) {
+				this.clientSocket = HttpServer.serverSocket.accept();
+
+				BufferedReader entrada = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+
+				String path = this.Solicitud(entrada);
+
+				OutputStream socketClient = clientSocket.getOutputStream();
+
+				if ("/webPage".equals(path)) {
 					webPage(socketClient);
-				}
-				else {
+				} else {
 					pageNotFound(socketClient);
-				}	
+				}
 				this.clientSocket.close();
-			}
-			catch (Exception e) {
+			} catch (Exception e) {
 				System.out.println(e.getMessage());
 				System.exit(1);
 			}
 		}
 	}
-	
+
 	/**
-	 * Method to attend users requests
-	 * @param entrada User request
-	 * @return The path of the request (web, image, ...)
+	 * 
+	 * @param rutaRequerida la ruta que desea solicitar el usuario.
+	 * @return retorna la ruta completa para resolver el direccionamiento
 	 */
-	public String getRequest(BufferedReader entrada) {
-		
-		boolean notExit=true;
-		String path=null;
-		
+	public String Solicitud(BufferedReader rutaRequerida) {
+		boolean fail = true;
+		String rutaCompl = null;
 		try {
-			while((this.serverMessage=entrada.readLine())!=null && notExit) {
-				
-				if(this.serverMessage.contains("GET")) {
-					String[]dir=this.serverMessage.split(" ");
-					path=dir[1];
-					notExit=false;
+			while ((this.mensaje = rutaRequerida.readLine()) != null && fail) {
+
+				if (this.mensaje.contains("GET")) {
+					String[] dir = this.mensaje.split(" ");
+					rutaCompl = dir[1];
+					fail = false;
 				}
 			}
-		} 
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
-		return path;
+		return rutaCompl;
 	}
-	
-	
+
 	/**
-	 * Method to attend the client web request
-	 * @param socketClient ClientSocket OutputStream
+	 * Metodo para resolver las solicitudes del cliente
+	 * @param socketClient 
 	 */
-	private void webPage (OutputStream socketClient) {
-        PrintWriter res=new PrintWriter(socketClient, true);
-        String outputLine="HTTP/1.1 200 \r\n\r\n<html><head><title>HTML con Imagen</title></head>"
-        +"<body style = \"background-color:rgb(150,206,209);\"><h1><center> HttpServer </center></h1>"
-        +"<div align = \"center\"><h2 style = \"color:darkred;\"> Imagen Iron Man </h2>"
-        +"<img src = \"https://vignette.wikia.nocookie.net/disney/images/9/96/Iron-Man-AOU-Render.png/revision/latest?cb=20180410032118&path-prefix=es\" width = \"500px\" height = \"500px\"></div></html></body>";
-        setOutput(res,outputLine,socketClient);
+	private void webPage(OutputStream socketClient) {
+		PrintWriter res = new PrintWriter(socketClient, true);
+		String outputLine = "HTTP/1.1 200 \r\n\r\n<html><head><title>HTML con Imagen</title></head>"
+				+ "<body style = \"background-color:rgb(150,206,209);\"><h1><center> HttpServer </center></h1>"
+				+ "<div align = \"center\"><h2 style = \"color:darkred;\"> Imagen Iron Man </h2>"
+				+ "<img src = \"https://vignette.wikia.nocookie.net/disney/images/9/96/Iron-Man-AOU-Render.png/revision/latest?cb=20180410032118&path-prefix=es\" width = \"500px\" height = \"500px\"></div></html></body>";
+		setOutput(res, outputLine, socketClient);
 	}
-    
-	
+
 	/**
-	 * Method to handle wrong client requests
+	 * Metodo para cuando las solicitudes del cliente no se encuentran. 
+	 * 
 	 * @param socketClient
 	 */
 	private void pageNotFound(OutputStream socketClient) {
+		PrintWriter res = new PrintWriter(socketClient, true);
+		String outputLine = "HTTP/1.1 404 \r\n\r\n<html><head><title>Pagina No Encontrada</title></head>"
+				+ "<body><center><h1> 404 </h1></center> <div align = \"center\"><h2> ERROR: Pagina No encontrada </h2></div></body></html>";
+		setOutput(res, outputLine, socketClient);
+	}
 
-        PrintWriter res = new PrintWriter(socketClient, true);
-        String outputLine="HTTP/1.1 404 \r\n\r\n<html><head><title>Pagina No Encontrada</title></head>"
-        +"<body><center><h1> 404 </h1></center> <div align = \"center\"><h2> ERROR: Pagina No encontrada </h2></div></body></html>";
-        setOutput(res,outputLine,socketClient);
-    }
-	
-	
 	/**
-	 * Method that give the response to the client request
-	 * @param res Response writer
-	 * @param outputLine String response (page)
+	 * Metodo para mostrar las solicitudes al cliente.
+	 * @param res Las respuestas que se deben enviar.
+	 * @param outputLine La pagina que se debe mostrar.
+	 * @param socketClient 
 	 */
 	private void setOutput(PrintWriter res, String outputLine, OutputStream socketClient) {
 		res.println(outputLine);
-        res.flush();
-        res.close();
+		res.flush();
+		res.close();
 
-        try {
+		try {
 			socketClient.flush();
 			socketClient.close();
-		} 
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
 	}
-	
-	/**
-	 * Method to close the server socket
-	 */
-	public void closeServerSocket() {
-		try {
-			this.servSocket.close();
-		} 
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * Method to establish the port
-	 * @return The port
-	 */
-	public static int getPort() {
-        if (System.getenv("PORT") != null) {
-            return new Integer(System.getenv("PORT"));
-        }
-        return 35000; 
-    }
 
 	/**
-	 * Method that starts the program
-	 * @param args No args
+	 * Metodo que establece el puerto para la conexion.
+	 * @return el puerto 35000
+	 */
+	public static int getPort() {
+		if (System.getenv("PORT") != null) {
+			return new Integer(System.getenv("PORT"));
+		}
+		return 35000;
+	}
+
+	/**
+	 * Programa principal
+	 * @param args
 	 */
 	public static void main(String args[]) {
-		HttpServer server=new HttpServer();
+		HttpServer server = new HttpServer();
 		System.out.println("Iniciando HttpServer");
-		server.startServer();
-		server.closeServerSocket();
+		server.run();
+		try {
+			serverSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
